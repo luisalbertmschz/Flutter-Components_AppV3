a9a439d406207f4ab58f0710ee105253cee22e3f
luisalbertmschz <luisalbertmsanchez@gmail.com>
2021-04-19 02:24:08 -0400

    🚀 Incorporando Método Map en Listas y LisTile.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:components_app_v3/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
+
+
  
 class MyApp extends StatelessWidget {
   @override
