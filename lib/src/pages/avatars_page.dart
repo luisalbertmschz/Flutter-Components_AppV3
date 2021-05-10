fdef89bd92c2a0ec25b988c108811dbc5772637a
luisalbertmschz <luisalbertmsanchez@gmail.com>
2021-05-10 02:12:46 -0400

    🚤 Navegación estática entre pantallas.

---
+++ lib/src/pages/avatars_page.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class AvatarsPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: Text('👤 Avatars Page'),
+        backgroundColor: Colors.blue[900],
+      
+      
+      ),
+  
+    );
+  }
+}
